8371f701f44fd8a99d41d8099865de09b24422be
Tommaso Siligardi <tomesse@outlook.it>
2021-03-12 19:39:40 +0100

    commit

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ const db = new sqlite3.Database("./database.db")
 
 /* db.get ->Restituisce il primo valore che trova nel db che rispecchi la query
   db.all -> Restituisce tutti i valori del db che soddisfano la query*/
+/* db.get(query, parametri, (err,result)=>{
+  if(err || !result){
+    res.status(401).json({ok: "false"})
+  }
+  else{
+    fai cose
+  }
+})*/
 
 app.get("/accreditamento", (req, res) => {
   res.json({
